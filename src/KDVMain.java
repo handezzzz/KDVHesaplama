0ec357b8abbee59177cc6a925395942530bedb8c
[handezzzz] <[colakogluhandenur@gmail.com]>
2023-03-30 22:15:05 +0300

    first commit

--- src/KDVMain.java
+++ src/KDVMain.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class KDVMain {
+import java.util.Scanner;
+
+public class KDVMain {
+    public static void main(String[] args) {
+        double tutar, kdvOran, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret tutarını giriniz: ");
+        tutar = input.nextDouble();
+
+        if(tutar <= 1000){
+            kdvOran = 0.18;
+        }
+
+        else{
+            kdvOran = 0.08;
+        }
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar: " + tutar);
+        System.out.println("KDV Oranı: " + kdvOran);
+        System.out.println("KDV Tutarı: " + kdvTutar);
+        System.out.println("KDV'li Tutarı: " + kdvliTutar);
+
+    }
 }
